17869262d7d2748ffcf790d068152105ae923925
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-09-14 22:40:40 -0300

    :sparkles: feat: pasta services criada com validações para os verbos GET sem parâmetro e com os parâmetro id e contato

---
+++ src/services/AlunoValidacoes.js
@@ -0,0 +1,22 @@
+import alunoDAO from "../DAO/alunoDAO";
+
+const Validacoes = {
+
+    _validaGet : async (id, callback)=>{
+        const aluno = await callback(id)
+        if(aluno === undefined){
+            throw new Error (`Aviso: ${id} não encontrado!`)
+        }else{
+            return aluno
+        }
+    },
+    _validaGetNome : async (nomeCompleto, callback)=>{
+        const aluno = await callback(nomeCompleto)
+        if(aluno === undefined){
+            throw new Error (`Aviso; ${nomeCompleto} não encontrado!`)
+        }else{
+            return aluno
+        }
+    }
+}
+export default Validacoes
